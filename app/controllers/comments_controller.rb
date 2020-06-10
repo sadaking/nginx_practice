cf7601bf2746f35f7112074c79782b86512f070e
sadaking <sichirigahama.37@gmail.com>
2020-06-10 22:44:34 +0900

    editアクションとupdateアクションを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,9 +10,34 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = @blog.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @comment = @blog.comments.find(params[:id])
+    respond_to do |format|
+      if @comment.update(comment_params)
+        flash.now[:notice] = 'コメントが編集されました'
+        format.js { render :index }
+      else
+        flash.now[:notice] = 'コメントの編集に失敗しました'
+        format.js { render :edit_error }
+      end
+    end
+  end
+
   private
 
   def comments_params
     params.require(:comment).permit(:blog_id, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
   end
 end
