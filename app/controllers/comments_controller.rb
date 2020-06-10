d0eb34a97951bdd823e579a4ef39abeddf7f3348
sadaking <sichirigahama.37@gmail.com>
2020-06-10 20:35:14 +0900

    コメントを保存、投稿するためのアクション

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.build(comments_params)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blog_path(@blog) }
+      else
+        format.html { redirect_to blog_path(@blog), notice: "投稿できませんでした。。。"}
+    end
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:blog_id, :content)
   end
 end
